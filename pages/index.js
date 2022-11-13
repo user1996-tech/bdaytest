c988ccf33252fdbe26f7f903a0a261aa8a750fda
Austin <madeinsg3@gmail.com>
2022-11-13 14:54:15 +0700

    test-scrollbar 2.0

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Invite from "./components/Invite";
 export default function Home() {
   const formRef = useRef(null);
   return (
-    <div className="scrollbar-thin">
+    <div className="scrollbar-thin scrollbar-thumb-[#DCB974]/40 scrollbar-track-[#031934]/40 ">
       <Invite formRef={formRef} />
       <Form formRef={formRef} />
     </div>
